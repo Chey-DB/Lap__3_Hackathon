56e37b1ce2466e8afd345db8532b261ed9c9d4a3
Chey-DB <cheydb@rocketmail.com>
2023-05-25 14:33:00 +0100

    created NavBar component and created styles.css for NavBar component and applied styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import { NavBar } from './components'
 function App() {
 
 
-  return <h1>App Component</h1>
+  return (
+    <>
+      <NavBar />
+      <h1>App Component</h1>
+  </>
+
+  )
 }
 
 export default App
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './styles.css'
+
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <main>
+        <header>
+            <nav>
+                <ul>
+                    <li className='logo'><a href="/"><img src="../../assets/superman_logo.png" alt="superman logo" /></a></li>
+                    <li className='searchBar'><input type="search" /></li>
+                </ul>
+            </nav>
+        </header>
+    </main>
   )
 }
 
